a5b2baa59f2a26e18a0458d62183efa1b08a8422
Kean Valgere E. Garcia <110815853+garciakean32@users.noreply.github.com>
2023-05-25 00:23:06 -0700

    Add files via upload

---
+++ uthangs login page/LoginPage.php
@@ -0,0 +1,18 @@
+<DOCTYPE!>
+    <html>
+
+    <head>
+        <title>Uthangs</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+
+    <body>
+        <h1 class="Header">Uthangs</h1>
+        <div class="container">
+            <h1>Owner Login</h1>
+            <input class="input" placeholder="Username"></input>
+            <input class="input" placeholder="Password"></input>
+            <button class="login-btn">Login</button>
+        </div>
+    </body>
+    <html>
